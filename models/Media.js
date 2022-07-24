343b3b4bb28de003f9634560861db1a0a58a5c05
Mafud Nur karim <40764354+MafudNk@users.noreply.github.com>
2022-07-24 19:11:58 +0700

    first commit

---
+++ models/Media.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const Media = sequelize.define("Media", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        createdAt: {
+            field: 'created_at',
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            field: 'updated_at',
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    },{ tableName: 'media' });
+
+    // Media.associate = models => {
+    //     Media.belongsTo(models.User, {
+    //         foreignKey: {
+    //             allowNull: false
+    //         }
+    //     });
+    // }
+
+    return Media;
+}
